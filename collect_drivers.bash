2474a8e92ea30107dd1373c0406b4d68416c2637
ilcadmin <ilcadmin@atlas04.ucsc.edu>
2016-03-29 00:00:02 -0700

    autocommit on Tue Mar 29 00:00:02 PDT 2016

--- collect_drivers.bash
+++ collect_drivers.bash
@@ -1,14 +1,1 @@
-#!/bin/bash
-frameworkdir=$HOME/ilc_primary/central_scipp_ilc_framework
-userfile=$HOME/admin_scripts/scipp_ilc_users_list.txt
-
-cd $frameworkdir/src/driver_collective
-for user in $(cat $userfile); do
-    if [[ ! -d $user ]]; then mkdir $user; fi;
-    cp -r /local-data/$user/scipp_ilc_framework/src/drivers $user
-    git add $user/*java
-done;
-
-cd $frameworkdir;
-git commit -am "autocommit on $(date)";
-git push origin master
+/export/home/ilcadmin/ilc_primary/admin_scripts/collect_drivers.bash
